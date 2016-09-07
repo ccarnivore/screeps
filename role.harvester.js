dba439212a7dba840a07ae3a9d1a6cf114cf1a65
roman <roman.robel@exitb.de>
2016-09-07 19:22:25 +0200

    * creep roles

--- role.harvester.js
+++ role.harvester.js
@@ -17,17 +17,28 @@ var roleHarvester = {
                                 || structure.structureType == STRUCTURE_TOWER
                                 || structure.structureType == STRUCTURE_CONTAINER
                                 || structure.structureType == STRUCTURE_STORAGE
-                                ) && structure.energy < structure.energyCapacity || (
-                                    structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] < structure.storeCapacity
-                                    );
+                                ) && structure.energy < structure.energyCapacity
+                        || (
+                            structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] < structure.storeCapacity
+                        );
                     }
             });
             
             if(targets.length > 0) {
-                
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
+            } else {
+                // creep should wait at spawn
+                var spawn = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => { return (structure.structureType == STRUCTURE_SPAWN) }
+                });
+
+                if (spawn.length > 0) {
+                    creep.moveTo(spawn[0]);
+
+                }
+
             }
         }
     }
