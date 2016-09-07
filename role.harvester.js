7c5f41d3cfa1b8370c5ad8185ee1e9d1caaef469
roman <roman.robel@exitb.de>
2016-09-07 20:53:19 +0200

    * creep roles

--- role.harvester.js
+++ role.harvester.js
@@ -11,13 +11,11 @@ var roleHarvester = {
         }
         else {
             var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => { return
-                        (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] < structure.storeCapacity) ||
-                        (structure.structureType == STRUCTURE_EXTENSION && structure.energy < structure.energyCapacity) ||
-                        (structure.structureType == STRUCTURE_SPAWN && structure.energy < structure.energyCapacity);
-                }
+                    filter: (structure) => { return structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] < structure.storeCapacity ||
+                (structure.structureType == STRUCTURE_EXTENSION && structure.energy < structure.energyCapacity) ||
+                (structure.structureType == STRUCTURE_SPAWN && structure.energy < structure.energyCapacity) }
             });
-            
+
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
@@ -37,7 +35,7 @@ var roleHarvester = {
             }
         }
     }
-    
+
 };
 
 module.exports = roleHarvester;
