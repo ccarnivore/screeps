9ed25f09c427af7e4ad9c35df6cb960af0cd7a54
roman <roman.robel@exitb.de>
2016-09-08 17:39:29 +0200

    * change repair order

--- main.towerController.js
+++ main.towerController.js
@@ -6,6 +6,10 @@ var towerController = {
      */
     defendRoom: function(room) {
         var hostiles = room.find(FIND_HOSTILE_CREEPS);
+        if (hostiles.length == 0) {
+            console.log('no hostile found');
+            return;
+        }
 
         if(hostiles.length > 0) {
             console.log('hostile creeps spotted');
@@ -16,11 +20,13 @@ var towerController = {
 
     healCreeps: function(room) {
         var injuredCreeps = room.find(FIND_CREEPS, {filter: (creep) => { return creep.hits < creep.hitsMax }});
-
-        if(injuredCreeps.length > 0) {
-            var towers = room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
-            towers.forEach(tower => {if (tower.energy >= 100) tower.heal(injuredCreeps[0]) });
+        if (injuredCreeps.length == 0) {
+            console.log('nobody hurt');
+            return;
         }
+
+        var towers = room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
+        towers.forEach(tower => {if (tower.energy >= 100) tower.heal(injuredCreeps[0]) });
     },
 
     repairStructures: function(room) {
