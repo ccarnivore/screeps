bb87a93f37db76cd0214a68bbb43adc72abdb7a4
roman <roman.robel@exitb.de>
2016-09-09 17:25:00 +0200

    * next level

--- main.creepHandling.js
+++ main.creepHandling.js
@@ -1,12 +1,3 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('main.creepHandling');
- * mod.thing == 'a thing'; // true
- */
-
 var level1 = 'level1',
     level2 = 'level2';
 
